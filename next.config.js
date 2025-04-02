3154edc241977d4eef8f03b167f475c24ec33451
fedor-ryzhenkov <fedor.ryzhenkov@gmail.com>
2025-04-02 04:50:20 +0400

    Remove standalone output config to make app compatible with custom server.js

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
   images: {
     domains: ['images.unsplash.com'],
     unoptimized: process.env.NODE_ENV === 'development',
