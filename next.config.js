6f41b6be4baa9d4fbe32548b618582243a9a0f33
fedor-ryzhenkov <fedor.ryzhenkov@gmail.com>
2025-04-02 04:36:32 +0400

    Convert next.config.js to ES module syntax

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-const createNextIntlPlugin = require('next-intl/plugin');
+import createNextIntlPlugin from 'next-intl/plugin';
 
 const withNextIntl = createNextIntlPlugin();
 
@@ -36,4 +36,4 @@ const nextConfig = {
   serverExternalPackages: ['@prisma/client', 'prisma'],
 }
 
-module.exports = withNextIntl(nextConfig); 
+export default withNextIntl(nextConfig); 
